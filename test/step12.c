7567f2aadbb6414a4d749c0411d372eb7792a155
Shota Yamamoto <kuaiwanga@gamil.com>
2022-03-08 12:48:23 +0900

    step12

--- test/step12.c
+++ test/step12.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stddef.h>
+#include <stdint.h>
 #include <signal.h>
 #include <unistd.h>
 
@@ -9,9 +9,9 @@
 #include "icmp.h"
 
 #include "driver/loopback.h"
+#include "driver/ether_tap.h"
 
 #include "test.h"
-#include "driver/ether_tap.h"
 
 static volatile sig_atomic_t terminate;
 
@@ -28,7 +28,6 @@ setup(void)
     struct net_device *dev;
     struct ip_iface *iface;
 
-    signal(SIGINT, on_signal);
     if (net_init() == -1) {
         errorf("net_init() failure");
         return -1;
@@ -38,7 +37,15 @@ setup(void)
         errorf("loopback_init() failure");
         return -1;
     }
-
+    iface = ip_iface_alloc(LOOPBACK_IP_ADDR, LOOPBACK_NETMASK);
+    if (!iface) {
+        errorf("ip_iface_alloc() failure");
+        return -1;
+    }
+    if (ip_iface_register(dev, iface) == -1) {
+        errorf("ip_iface_register() failure");
+        return -1;
+    }
     dev = ether_tap_init(ETHER_TAP_NAME, ETHER_TAP_HW_ADDR);
     if (!dev) {
         errorf("ether_tap_init() failure");
@@ -53,7 +60,6 @@ setup(void)
         errorf("ip_iface_register() failure");
         return -1;
     }
-
     if (net_run() == -1) {
         errorf("net_run() failure");
         return -1;
@@ -70,28 +76,14 @@ cleanup(void)
 int
 main(int argc, char *argv[])
 {
-    ip_addr_t src, dst;
-    uint16_t id, seq = 0;
-    size_t offset = IP_HDR_SIZE_MIN + ICMP_HDR_SIZE;
-
     signal(SIGINT, on_signal);
     if (setup() == -1) {
         errorf("setup() failure");
         return -1;
     }
-    ip_addr_pton(LOOPBACK_IP_ADDR, &src);
-    dst = src;
-    id = getpid() % UINT16_MAX;
-
     while (!terminate) {
-        // if (ip_output(IP_PROTOCOL_ICMP, test_data + offset, sizeof(test_data) - offset, src, dst) == -1) {
-        if (icmp_output(ICMP_TYPE_ECHO, 0, hton32(id << 16 | ++seq), test_data + offset, sizeof(test_data) - offset, src, dst) == -1) {
-            errorf("icmp_output() failure");
-            break;
-        }
         sleep(1);
     }
-    
     cleanup();
     return 0;
 }
